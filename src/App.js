fb3806a5a7a5c6e52782b94d5e76eb154f02382b
Alwin Sebastian <alwinseb01@gmail.com>
2021-09-28 21:15:07 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import './App.css';
 
 function App() {
+
+  const [imageLink, setImageLink] = useState("");
+  const [emotions, setEmotions] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  function fetchData(){
+    fetch("https://facial-emotion-recognition.p.rapidapi.com/cloudVision/facialEmotionRecognition?source=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1527631120902-378417754324%3Fixlib%3Drb-1.2.1%26ixid%3DeyJhcHBfaWQiOjEyMDd9%26auto%3Dformat%26fit%3Dcrop%26w%3D2250%26q%3D80&sourceType=url", 
+    {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "x-rapidapi-host": "facial-emotion-recognition.p.rapidapi.com",
+      "x-rapidapi-key": "2afc1e1754msh804a74f12359d08p120586jsndc64f676a563"
+    },
+    body: JSON.stringify( {
+      "source": imageLink,
+      "sourceType": "url"
+    })
+  })
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data);
+    setEmotions(data.emotions[0]);
+    console.log(emotions);
+    setIsLoading(false);
+  })
+  .catch((err)=>{
+    console.error(err);
+  });
+
+  }
+  
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Emotion Recognition</h1>
+    <form onSubmit={(event) => {
+      event.preventDefault();
+      fetchData();
+      setImageLink("");
+      setIsLoading(true);
+    }} action="">
+      
+      <input 
+      onChange={(event) => setImageLink(event.target.value)}
+      type="url" name="url" className="search-input" id="url" placeholder="https://..."/>  
+      <input className="btn" type="submit" value="Send Request"/>
+    </form>
+    {isLoading ? (
+        ""
+      ) : (
+        <div className="data">
+          <p>
+            Joy: <span>{emotions.joyLikelihood}</span>
+          </p>
+          <p>
+            Anger: <span>{emotions.angerLikelihood}</span>
+          </p>
+          <p>
+            Suprise: <span>{emotions.surpriseLikelihood}</span>
+          </p>
+          <p>
+            Sorrow: <span>{emotions.sorrowLikelihood}</span>
+          </p>
+        </div>
+      )}
     </div>
   );
 }
+
+
 
 export default App;
